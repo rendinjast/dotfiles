359e73ea0c08dd9aa3da301effbae3b14147c529
Erfan Khadivar <rendinjast@gmail.com>
2024-05-30 10:25:22 +0330

    clean up

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"html","javascript","typescript","jsdoc","json","tsx","scss","python"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
